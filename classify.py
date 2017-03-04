0906c475358a5f1c02d5079ed06c23a86853ff7e
Tang Yixuan <yxxy@195-234.priv23.nus.edu.sg>
2017-03-04 16:55:46 +0800

    read in data
    resized to (100,100)
    random split into train and evaluation set

--- classify.py
+++ classify.py
@@ -1,10 +1,22 @@
 import models as md
 from utils import *
 
-X_train = np.zeros([2,3])
-print X_train
-# Y_train = np.
+
+train_path = '../data/train'
+test_path = '../data/test1'
+width = 100
+height = 100
+
+data = read_images(train_path, height = height, width = width)
+X_train, Y_train, X_eval, Y_eval = data
+print X_train.shape, Y_train.shape, X_eval.shape, Y_eval.shape
+# X_test, Y_test = read_images(test_path, height = height, width = width, is_train = False)
+# print X_test.shape, Y_test.shape
+
+'''
 model = md.get_vgg()
 md.print_model(model)
 
 # model.fit(X_train, Y_train, batch_size=32, nb_epoch=1)
+
+'''
--- utils.py
+++ utils.py
@@ -1,1 +1,30 @@
+import os
 import numpy as np
+from scipy.misc import imread, imresize
+from sklearn.cross_validation import train_test_split
+
+def read_images(path, height = 100, width = 100, is_train = True):
+	fn_list = os.listdir(path)
+	X = []
+	Y = []
+	for fn in fn_list:
+		if fn[-3:] == 'jpg':
+			# read in image
+			file_path = os.path.join(path, fn)
+			image = imread(file_path)
+			image = imresize(image, (height, width))
+			X.append(image)
+		
+			# read in label
+			if is_train:
+				label = fn[:fn.find('.')]
+				if label == 'cat':
+					Y.append(1)
+				if label == 'dog':
+					Y.append(0)
+
+			# for debug
+			if len(X)>99:
+				break
+
+	return train_test_split(np.array(X), np.array(Y), test_size=0.2, random_state=27)
